c22e77cd18e6b326c06333e4d1a978e353a69a94
bonjin6770 <bonjin6770@gmail.com>
2016-12-28 23:09:13 +0900

    add solutio

---
+++ DegarashiShop/DegarashiShop/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DegarashiShop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.purchaseButton = new System.Windows.Forms.Button();
+            this.itemListBox = new System.Windows.Forms.ListBox();
+            this.addToCartButton = new System.Windows.Forms.Button();
+            this.cartListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MyricaM M", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "商品一覧";
+            // 
+            // purchaseButton
+            // 
+            this.purchaseButton.Font = new System.Drawing.Font("MyricaM M", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.purchaseButton.Location = new System.Drawing.Point(660, 294);
+            this.purchaseButton.Name = "purchaseButton";
+            this.purchaseButton.Size = new System.Drawing.Size(178, 44);
+            this.purchaseButton.TabIndex = 2;
+            this.purchaseButton.Text = "購入";
+            this.purchaseButton.UseVisualStyleBackColor = true;
+            this.purchaseButton.Click += new System.EventHandler(this.purchaseButton_Click);
+            // 
+            // itemListBox
+            // 
+            this.itemListBox.Font = new System.Drawing.Font("MyricaM M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.itemListBox.FormattingEnabled = true;
+            this.itemListBox.ItemHeight = 16;
+            this.itemListBox.Location = new System.Drawing.Point(12, 32);
+            this.itemListBox.Name = "itemListBox";
+            this.itemListBox.Size = new System.Drawing.Size(404, 260);
+            this.itemListBox.TabIndex = 3;
+            // 
+            // addToCartButton
+            // 
+            this.addToCartButton.Font = new System.Drawing.Font("MyricaM M", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.addToCartButton.Location = new System.Drawing.Point(238, 294);
+            this.addToCartButton.Name = "addToCartButton";
+            this.addToCartButton.Size = new System.Drawing.Size(178, 44);
+            this.addToCartButton.TabIndex = 4;
+            this.addToCartButton.Text = "カートに入れる";
+            this.addToCartButton.UseVisualStyleBackColor = true;
+            this.addToCartButton.Click += new System.EventHandler(this.addToCartButton_Click);
+            // 
+            // cartListBox
+            // 
+            this.cartListBox.Font = new System.Drawing.Font("MyricaM M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cartListBox.FormattingEnabled = true;
+            this.cartListBox.ItemHeight = 16;
+            this.cartListBox.Location = new System.Drawing.Point(434, 32);
+            this.cartListBox.Name = "cartListBox";
+            this.cartListBox.Size = new System.Drawing.Size(404, 260);
+            this.cartListBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MyricaM M", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(430, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "カート";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cartListBox);
+            this.Controls.Add(this.addToCartButton);
+            this.Controls.Add(this.itemListBox);
+            this.Controls.Add(this.purchaseButton);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "でがらし商会";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button purchaseButton;
+        private System.Windows.Forms.ListBox itemListBox;
+        private System.Windows.Forms.Button addToCartButton;
+        private System.Windows.Forms.ListBox cartListBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DegarashiShop/DegarashiShop/MainForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace DegarashiShop
+{
+    public partial class MainForm : Form
+    {
+        // カート(key = ID, value = count)
+        Dictionary<int, int> _cart = new Dictionary<int, int>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void purchaseButton_Click(object sender, EventArgs e)
+        {
+            // 画面で選択した商品のリストを取得
+            Dictionary<int, int> cart = _cart;
+
+            // 購入できない商品リストの取得
+            Dictionary<int, int> limitOverItems = GetLimitOverItems(cart);
+
+            // 購入できない商品リストのメッセージ表示
+            ShowMessageLimiteOverItems(limitOverItems);
+
+            // 購入可能な商品を購入リストを取得
+            Dictionary<int, int> purchaseItems = GetPurchaseItems(cart);
+
+            // 購入する商品をデータベースへ保存
+            SavePurchaseItems(purchaseItems);
+
+            // 購入する商品への処理が完了したメッセージ表示
+            ShowMessagePurchaseItems(purchaseItems);
+
+        }
+
+        private void ShowMessageLimiteOverItems(Dictionary<int, int> limitOverItems)
+        {
+            foreach (var id in limitOverItems)
+            {
+                string itemName = itemListBox.Items[id.Key - 1].ToString();
+
+                string errorMessage = String.Format("商品一つにつき、ご購入できるのは１０個までです。{0}【{1}】は１０個以下にしてください。", System.Environment.NewLine, itemName);
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        private void SavePurchaseItems(Dictionary<int, int> purchaseItems)
+        {
+
+            // ２．データベースへの接続処理
+            string dbfile = "degarashi-shop.db";
+            using (var conn = new SQLiteConnection("Data Source=" + dbfile))
+            {
+                conn.Open();
+                using (SQLiteTransaction sqlt = conn.BeginTransaction())
+                {
+                    using (SQLiteCommand command = conn.CreateCommand())
+                    {
+                        foreach (var id in purchaseItems)
+                        {
+                            string itemName = itemListBox.Items[id.Key - 1].ToString();
+                            int itemCount = id.Value;
+
+                            // ５．データベースへ購入情報の保存
+                            command.CommandText = "insert into purchase (item_id) values('" + id.Key + "')";
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    sqlt.Commit();
+                }
+                conn.Close();
+            }
+        }
+
+        private void ShowMessagePurchaseItems(Dictionary<int, int> purchaseItems)
+        {
+            // １．画面表示用の文言の準備
+            var sb = new StringBuilder();
+            sb.AppendLine("次の商品の購入処理を完了しました。");
+            sb.AppendLine("===========================================");
+            foreach (var id in purchaseItems)
+            {
+                string itemName = itemListBox.Items[id.Key - 1].ToString();
+                int itemCount = id.Value;
+                // ４．購入処理終了後、画面へ表示する文言の組み立て
+                var selectedItem = String.Format("「{0} ({1}個)」", itemName, itemCount);
+                sb.AppendLine(selectedItem);
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        private Dictionary<int, int> GetPurchaseItems(Dictionary<int, int> cart)
+        {
+            Dictionary<int, int> purchaseItems = new Dictionary<int, int>();
+            foreach (var item in cart)
+            {
+                int itemCount = item.Value;
+
+                // ３．一つの商品を１０個以上購入することはできないので、そのチェック処理
+                if (itemCount > 10)
+                {
+                    // 購入できない商品は飛ばす。
+                    continue;
+                }
+
+                // 購入可能な商品を購入リストへ追加
+                purchaseItems.Add(item.Key, item.Value);
+            }
+
+            return purchaseItems;
+        }
+
+        private Dictionary<int, int> GetLimitOverItems(Dictionary<int, int> cart)
+        {
+            Dictionary<int, int> limitOverItems = new Dictionary<int, int>();
+            foreach (var item in cart)
+            {
+                int itemCount = item.Value;
+
+                // ３．一つの商品を１０個以上購入することはできないので、そのチェック処理
+                if (itemCount > 10)
+                {
+                    limitOverItems.Add(item.Key, item.Value);
+                }
+            }
+
+            return limitOverItems;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            // データベースから商品一覧を取得
+            string dbfile = "degarashi-shop.db";
+            using (var conn = new SQLiteConnection("Data Source=" + dbfile))
+            {
+                conn.Open();
+                using (SQLiteCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM items order by id";
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            itemListBox.Items.Add(reader["name"].ToString());
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        private void addToCartButton_Click(object sender, EventArgs e)
+        {
+            // 購入処理
+            var id = itemListBox.SelectedIndex + 1;
+
+            if (_cart.ContainsKey(id))
+            {
+                // すでにカートに入っている種類の商品であれば終了を追加する
+                _cart[id] = _cart[id] + 1;
+            }
+            else
+            {
+                // まだカートにない種類の商品であれば新規に追加する
+                _cart.Add(id, 1);
+            }
+
+            // カートの中身を表示
+            cartListBox.Items.Clear();
+            foreach (var each in _cart)
+            {
+                var selectedItem = itemListBox.Items[each.Key - 1].ToString() + @" (" + each.Value.ToString() + @"個)";
+                cartListBox.Items.Add(selectedItem);
+            }
+        }
+    }
+}
